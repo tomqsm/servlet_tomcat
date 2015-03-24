5572108e6b2c7d8e2f4c6b3ba2d355dd55df0e72
Tomasz Kusmierczyk <toks@TOKS-PC2.MobicaPL.local>
2015-03-24 12:14:23 +0100

    improve project setup

--- src/main/webapp/js/initialise-widgets.js
+++ src/main/webapp/js/initialise-widgets.js
@@ -2,4 +2,10 @@ var app = (function f($, doc, _) {
     $(doc).on("focus", "#menu li.selected", function() {
         console.log('initialise something on focus');
     });
+    $(doc).ready(function() {
+        initialiseWidges();
+    });
+    function initialiseWidges(){
+        var w = $('#showSize').showSize();
+    }
 })($, document, _);
